1702a2babbc5ad2f821e228b59301c98b779c042
Gong Ke Gouldstone <fifo@fifo-VirtualBox.(none)>
2013-11-26 22:46:13 -0500

    initial commit.  Says hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnagabySite::Application.config.secret_key_base = 'd32fa94eae8e9a614b9755565244f199e65855a91f5c9ff663251d85809de3b8c465c82365c835320f7807dcd0cf58e811eaf39f8e7e087462d52eaa8336a908'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SnagabySite::Application.config.session_store :cookie_store, key: '_snagaby_site_session'
